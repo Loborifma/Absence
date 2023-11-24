b087e84563dc32eb1eb3d82c7cb875249990bbe6
Maksim Finkelshtein <90143196+Loborifma@users.noreply.github.com>
2023-11-24 07:23:45 +0000

    Update 3 files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import Table from "../mySolutionMUI/Table/Table";
 
 function App() {
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
         <Table />
-      </DndProvider>
     </div>
   );
 }
